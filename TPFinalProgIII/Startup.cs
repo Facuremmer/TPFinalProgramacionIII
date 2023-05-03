a6f69913ac96a92cf7424575e0ea3b1127b53dac
Facundo <facundoremmer0@gmail.com>
2023-05-02 21:39:39 -0300

    Le doy permiso al puerto 4200 para que pueda acceder y no me de problema de CORS.

--- TPFinalProgIII/Startup.cs
+++ TPFinalProgIII/Startup.cs
@@ -29,6 +29,7 @@ namespace TPFinalProgIII
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddAutoMapper(typeof(Startup));
             services.AddControllers();
 
@@ -61,6 +62,13 @@ namespace TPFinalProgIII
         //VER COMO ARREGLAR PARA PODER USAR SWAGGER, DE ULTIMA FIJARME EN EL PROYECTO QUE ARRANCAMOS EL AÑO PASADO.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options =>
+            {
+                options.WithOrigins("http://localhost:4200");
+                options.AllowAnyMethod();
+                options.AllowAnyHeader();
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
